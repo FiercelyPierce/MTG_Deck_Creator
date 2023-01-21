945457d6767ddb2dccb07128d28e3f96f1e4658d
FiercelyPierce <cir21001@byui.edu>
2023-01-20 21:49:03 -0700

    Added an index

--- mtgsite/mysite/settings.py
+++ mtgsite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'mtg.apps.MtgConfig'
+    'mtg.apps.MtgConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
